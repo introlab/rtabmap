7c85f11fbba19753cfbcbf693c8395dbf5240f14
matlabbe <matlabbe@gmail.com>
2015-09-09 21:09:51 -0400

    fixed goal events sent by DBReader

--- corelib/src/DBReader.cpp
+++ corelib/src/DBReader.cpp
@@ -205,19 +205,19 @@ void DBReader::mainLoop()
 					double delay = stamp - previousStamp;
 					UWARN("Goal %d detected, posting it! Waiting %f seconds before sending next data...",
 							goalId, delay);
-					this->post(new RtabmapEventCmd(RtabmapEventCmd::kCmdGoal, "", goalId));
+					this->post(new RtabmapEventCmd(RtabmapEventCmd::kCmdGoal, goalId));
 					uSleep(delay*1000);
 				}
 				else
 				{
 					UWARN("Goal %d detected, posting it!", goalId);
-					this->post(new RtabmapEventCmd(RtabmapEventCmd::kCmdGoal, "", goalId));
+					this->post(new RtabmapEventCmd(RtabmapEventCmd::kCmdGoal, goalId));
 				}
 			}
 			else
 			{
 				UWARN("Goal %d detected, posting it!", goalId);
-				this->post(new RtabmapEventCmd(RtabmapEventCmd::kCmdGoal, "", goalId));
+				this->post(new RtabmapEventCmd(RtabmapEventCmd::kCmdGoal, goalId));
 			}
 		}
 
