5d1ea43b9e1e4454b8d5bc4007d34536fb3e38ef
matlabbe <matlabbe@gmail.com>
2016-10-24 14:44:49 -0400

    Fixed build error on Windows

--- guilib/src/ExportCloudsDialog.cpp
+++ guilib/src/ExportCloudsDialog.cpp
@@ -1295,7 +1295,7 @@ bool ExportCloudsDialog::getExportedClouds(
 						{
 							// When not saving to file, tex_coordinates should be linked to points, not polygon vertices
 							int nPoints = textureMesh->cloud.data.size()/textureMesh->cloud.point_step;
-							std::vector<Eigen::Vector2f> tmpCoordinates = textureMesh->tex_coordinates[0];
+							std::vector<Eigen::Vector2f, Eigen::aligned_allocator<Eigen::Vector2f> > tmpCoordinates = textureMesh->tex_coordinates[0];
 							textureMesh->tex_coordinates[0].resize(nPoints);
 
 							int polygonSize = textureMesh->tex_polygons[0][0].vertices.size();
