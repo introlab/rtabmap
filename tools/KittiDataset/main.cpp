6f9ac4b88b67889e5465a69b44748560618a821c
matlabbe <matlabbe@gmail.com>
2017-05-23 17:30:23 -0400

    fixed some compilation warnings

--- tools/KittiDataset/main.cpp
+++ tools/KittiDataset/main.cpp
@@ -259,20 +259,19 @@ int main(int argc, char * argv[])
 	cv::Mat_<double> P1(3,4);
 	cv::Mat_<double> P2(3,4);
 	cv::Mat_<double> P3(3,4);
-	char skipStr[10];
-	fscanf (pFile, "%s %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf", skipStr,
+	fscanf (pFile, "%*s %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf",
 			&P0(0, 0), &P0(0, 1), &P0(0, 2), &P0(0, 3),
 			&P0(1, 0), &P0(1, 1), &P0(1, 2), &P0(1, 3),
 			&P0(2, 0), &P0(2, 1), &P0(2, 2), &P0(2, 3));
-	fscanf (pFile, "%s %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf", skipStr,
+	fscanf (pFile, "%*s %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf",
 			&P1(0, 0), &P1(0, 1), &P1(0, 2), &P1(0, 3),
 			&P1(1, 0), &P1(1, 1), &P1(1, 2), &P1(1, 3),
 			&P1(2, 0), &P1(2, 1), &P1(2, 2), &P1(2, 3));
-	fscanf (pFile, "%s %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf", skipStr,
+	fscanf (pFile, "%*s %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf",
 			&P2(0, 0), &P2(0, 1), &P2(0, 2), &P2(0, 3),
 			&P2(1, 0), &P2(1, 1), &P2(1, 2), &P2(1, 3),
 			&P2(2, 0), &P2(2, 1), &P2(2, 2), &P2(2, 3));
-	fscanf (pFile, "%s %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf", skipStr,
+	fscanf (pFile, "%*s %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf",
 			&P3(0, 0), &P3(0, 1), &P3(0, 2), &P3(0, 3),
 			&P3(1, 0), &P3(1, 1), &P3(1, 2), &P3(1, 3),
 			&P3(2, 0), &P3(2, 1), &P3(2, 2), &P3(2, 3));
