11c34d383d2b805f61b484eeb105f0a1f78a39fe
Patrick José Pereira <patrickelectric@gmail.com>
2018-05-26 21:40:24 -0300

    PreferencesDialog: Add forgotten include
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- guilib/src/PreferencesDialog.cpp
+++ guilib/src/PreferencesDialog.cpp
@@ -35,6 +35,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <QtCore/QTimer>
 #include <QUrl>
 
+#include <QButtonGroup>
 #include <QFileDialog>
 #include <QInputDialog>
 #include <QMessageBox>
