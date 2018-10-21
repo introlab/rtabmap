introlab/rtabmap

commit 39e1d453697b7cdd9dd11038f6d9d998b8a8ed53

Fixed build with OpenCV4 (#322)

corelib/src/camera/CameraOpenNICV.cpp
@@ -26,6 +26,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <rtabmap/core/camera/CameraOpenNICV.h>
 #include <rtabmap/utilite/UTimer.h>
+#include <opencv2/videoio/videoio_c.h>
 
 namespace rtabmap
 {

corelib/src/camera/CameraStereoVideo.cpp
@@ -28,6 +28,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <rtabmap/core/camera/CameraStereoVideo.h>
 #include <rtabmap/utilite/UTimer.h>
 #include <rtabmap/utilite/UConversion.h>
+#include <opencv2/videoio/videoio_c.h>
+#include <opencv2/imgproc/types_c.h>
 
 namespace rtabmap
 {

corelib/src/camera/CameraVideo.cpp
@@ -28,6 +28,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <rtabmap/core/camera/CameraVideo.h>
 #include <rtabmap/utilite/UTimer.h>
 #include <rtabmap/utilite/UConversion.h>
+#include <opencv2/videoio/videoio_c.h>
 
 namespace rtabmap
 {

corelib/src/Features2d.cpp
@@ -948,6 +948,8 @@ void ORB::parseParameters(const ParametersMap & parameters)
 	{
 #if CV_MAJOR_VERSION < 3
 		_orb = cv::Ptr<CV_ORB>(new CV_ORB(this->getMaxFeatures(), scaleFactor_, nLevels_, edgeThreshold_, firstLevel_, WTA_K_, scoreType_, patchSize_, parameters));
+#elif CV_MAJOR_VERSION > 3
+		_orb = CV_ORB::create(this->getMaxFeatures(), scaleFactor_, nLevels_, edgeThreshold_, firstLevel_, WTA_K_, (cv::ORB::ScoreType)scoreType_, patchSize_, fastThreshold_);
 #else
 		_orb = CV_ORB::create(this->getMaxFeatures(), scaleFactor_, nLevels_, edgeThreshold_, firstLevel_, WTA_K_, scoreType_, patchSize_, fastThreshold_);
 #endif
@@ -1548,7 +1550,9 @@ void KAZE::parseParameters(const ParametersMap & parameters)
 	Parameters::parse(parameters, Parameters::kKAZENOctaveLayers(), nOctaveLayers_);
 	Parameters::parse(parameters, Parameters::kKAZEDiffusivity(), diffusivity_);
 
-#if CV_MAJOR_VERSION > 2
+#if CV_MAJOR_VERSION > 3
+	kaze_ = cv::KAZE::create(extended_, upright_, threshold_, nOctaves_, nOctaveLayers_, (cv::KAZE::DiffusivityType)diffusivity_);
+#elif CV_MAJOR_VERSION > 2
 	kaze_ = cv::KAZE::create(extended_, upright_, threshold_, nOctaves_, nOctaveLayers_, diffusivity_);
 #else
 	UWARN("RTAB-Map is not built with OpenCV3 so Kaze feature cannot be used!");

corelib/src/camera/CameraFreenect.cpp
@@ -28,6 +28,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <rtabmap/utilite/UThread.h>
 #include <rtabmap/utilite/UTimer.h>
 #include <rtabmap/core/util2d.h>
+#include <opencv2/imgproc/types_c.h>
 
 #ifdef RTABMAP_FREENECT
 #include <libfreenect.h>

corelib/src/camera/CameraFreenect2.cpp
@@ -29,6 +29,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <rtabmap/utilite/UMath.h>
 #include <rtabmap/utilite/UTimer.h>
 #include <rtabmap/core/util2d.h>
+#include <opencv2/imgproc/types_c.h>
 
 #ifdef RTABMAP_FREENECT2
 #include <libfreenect2/libfreenect2.hpp>
@@ -429,11 +430,11 @@ SensorData CameraFreenect2::captureImage(CameraInfo * info)
 					cv::Mat rgbMat; // rtabmap uses 3 channels RGB
 					#ifdef LIBFREENECT2_WITH_TEGRAJPEG_SUPPORT 
 
-					 cv::cvtColor(rgbMatC4, rgbMat, CV_RGBA2BGR); 
+					 cv::cvtColor(rgbMatC4, rgbMat, CV_RGBA2BGR);
 
 					#else 
 
-					cv::cvtColor(rgbMatC4, rgbMat, CV_BGRA2BGR); 
+					cv::cvtColor(rgbMatC4, rgbMat, CV_BGRA2BGR);
 
 					#endif 
 					cv::flip(rgbMat, rgb, 1);

corelib/src/camera/CameraImages.cpp
@@ -33,6 +33,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <rtabmap/core/util3d.h>
 #include <rtabmap/core/util3d_filtering.h>
 #include <rtabmap/core/Graph.h>
+#include <opencv2/imgproc/types_c.h>
 #include <fstream>
 
 namespace rtabmap

corelib/src/camera/CameraK4W2.cpp
@@ -28,6 +28,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <rtabmap/utilite/UTimer.h>
 #include <rtabmap/utilite/UThreadC.h>
 #include <rtabmap/core/util2d.h>
+#include <opencv2/imgproc/types_c.h>
 
 #ifdef RTABMAP_K4W2
 #include <Kinect.h>

commit 93a3a667c811f64886f668bc9d372d93cb231964

fixed build errors with OpenCV2 caused by commit https://github.com/introlab/rtabmap/commit/39e1d453697b7cdd9dd11038f6d9d998b8a8ed53 (OpenCV4)

corelib/src/camera/CameraOpenNICV.cpp
@@ -26,7 +26,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <rtabmap/core/camera/CameraOpenNICV.h>
 #include <rtabmap/utilite/UTimer.h>
+#if CV_MAJOR_VERSION > 3
 #include <opencv2/videoio/videoio_c.h>
+#endif
 
 namespace rtabmap
 {

corelib/src/camera/CameraStereoVideo.cpp
@@ -28,8 +28,10 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <rtabmap/core/camera/CameraStereoVideo.h>
 #include <rtabmap/utilite/UTimer.h>
 #include <rtabmap/utilite/UConversion.h>
-#include <opencv2/videoio/videoio_c.h>
 #include <opencv2/imgproc/types_c.h>
+#if CV_MAJOR_VERSION > 3
+#include <opencv2/videoio/videoio_c.h>
+#endif
 
 namespace rtabmap
 {

corelib/src/camera/CameraVideo.cpp
@@ -28,7 +28,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <rtabmap/core/camera/CameraVideo.h>
 #include <rtabmap/utilite/UTimer.h>
 #include <rtabmap/utilite/UConversion.h>
+#if CV_MAJOR_VERSION > 3
 #include <opencv2/videoio/videoio_c.h>
+#endif
 
 namespace rtabmap
 {