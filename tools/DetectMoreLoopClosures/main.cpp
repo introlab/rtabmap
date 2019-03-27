81313c32eedcc58f498ab802c1a37930a923071e
matlabbe <matlabbe@gmail.com>
2019-03-27 16:12:49 -0400

    detectMoreLoopClosures tool: Fixed windows build

--- tools/DetectMoreLoopClosures/main.cpp
+++ tools/DetectMoreLoopClosures/main.cpp
@@ -40,6 +40,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <pcl/common/common.h>
 #include <pcl/surface/poisson.h>
 #include <stdio.h>
+#include <signal.h>
 
 using namespace rtabmap;
 
