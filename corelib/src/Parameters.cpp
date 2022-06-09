bc42bc35205972c24ce7e27d262bca8e40e3c286
mathieu86 <mathieu@robust.ai>
2022-06-09 10:51:08 -0700

    Added Kp/ByteToFloat parameter to odom parameters

--- corelib/src/Parameters.cpp
+++ corelib/src/Parameters.cpp
@@ -188,7 +188,8 @@ rtabmap::ParametersMap Parameters::getDefaultOdometryParameters(bool stereo, boo
 			group.compare("GTSAM") == 0 ||
 			(vis && (group.compare("Vis") == 0 || group.compare("PyMatcher") == 0 || group.compare("GMS") == 0)) ||
 			iter->first.compare(kRtabmapPublishRAMUsage())==0 ||
-			iter->first.compare(kRtabmapImagesAlreadyRectified())==0)
+			iter->first.compare(kRtabmapImagesAlreadyRectified())==0 ||
+			iter->first.compare(kKpByteToFloat())==0)
 		{
 			odomParameters.insert(*iter);
 		}
