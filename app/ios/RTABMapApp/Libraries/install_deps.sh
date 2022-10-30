def876983d7c1411023c8387e6e5a428d79eebb4
matlabbe <matlabbe@gmail.com>
2022-10-30 15:07:55 -0700

    docker/android: fixed docker not detecting compilation failure. ios: added exit flag for install_deps.sh script

--- app/ios/RTABMapApp/Libraries/install_deps.sh
+++ app/ios/RTABMapApp/Libraries/install_deps.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 # Tested on Apple Silicon Mac, with cmake 3.19.2.
 
 pwd=$(pwd)
--- docker/bionic/android/deps.bash
+++ docker/bionic/android/deps.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [ $# -ne 1 ]; then
     echo "deps.bash android_install_prefix   # Example: setup.bash /opt/android"
     exit 1
