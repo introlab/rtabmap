d19bc23462a65bef8836bd859df31bc532f3b349
Borong Yuan <yuanborong@hotmail.com>
2024-04-13 18:25:52 +0800

    copy global descriptor

--- corelib/src/camera/CameraDepthAI.cpp
+++ corelib/src/camera/CameraDepthAI.cpp
@@ -800,7 +800,7 @@ SensorData CameraDepthAI::captureImage(CameraInfo * info)
 
 		data.setFeatures(keypoints, std::vector<cv::Point3f>(), descriptors);
 		if(detectFeatures_ == 3)
-			data.addGlobalDescriptor(GlobalDescriptor(1, cv::Mat(1, global_descriptor.size(), CV_32FC1, global_descriptor.data())));
+			data.addGlobalDescriptor(GlobalDescriptor(1, cv::Mat(1, global_descriptor.size(), CV_32FC1, global_descriptor.data()).clone()));
 	}
 
 #else
