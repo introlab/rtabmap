3d9f0c159f8018fbbcf80aab2906042019d5ed6c
matlabbe <matlabbe@gmail.com>
2024-04-14 13:18:10 -0700

    reverted camera tool

--- tools/Camera/main.cpp
+++ tools/Camera/main.cpp
@@ -1,234 +1,198 @@
 /*
- * Software License Agreement (BSD License)
- *
- *  Point Cloud Library (PCL) - www.pointclouds.org
- *  Copyright (c) 2015-, Open Perception, Inc.
- *  Copyright (c) 2015, The MITRE Corporation
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *
- * Author: Keven Ring <keven@mitre.org>
- */
+Copyright (c) 2010-2016, Mathieu Labbe - IntRoLab - Universite de Sherbrooke
+All rights reserved.
 
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-#include <pcl/common/time.h> //fps calculations
-#include <pcl/io/hdl_grabber.h>
-#include <pcl/io/vlp_grabber.h>
-#include <pcl/visualization/point_cloud_color_handlers.h>
-#include <pcl/visualization/cloud_viewer.h>
-#include <pcl/visualization/image_viewer.h>
-#include <pcl/console/parse.h>
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the Universite de Sherbrooke nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
 
-#include <boost/algorithm/string.hpp>
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
-#include <mutex>
-#include <string>
+#include "rtabmap/core/CameraRGB.h"
+#include "rtabmap/core/DBReader.h"
+#include "rtabmap/utilite/ULogger.h"
+#include "rtabmap/utilite/UFile.h"
+#include "rtabmap/utilite/UDirectory.h"
+#include "rtabmap/utilite/UConversion.h"
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/highgui/highgui_c.h>
+#include <stdio.h>
 
-using namespace std::chrono_literals;
-using namespace pcl;
-using namespace pcl::console;
-using namespace pcl::visualization;
-
-#define SHOW_FPS 0
-#if SHOW_FPS
-#define FPS_CALC(_WHAT_) \
-do \
-{ \
-    static unsigned count = 0;\
-    static double last = getTime ();\
-    double now = getTime (); \
-    ++count; \
-    if (now - last >= 1.0) \
-    { \
-      std::cout << "Average framerate("<< _WHAT_ << "): " << double(count)/double(now - last) << " Hz" <<  std::endl; \
-      count = 0; \
-      last = now; \
-    } \
-}while(false)
-#else
-#define FPS_CALC(_WHAT_) \
-do \
-{ \
-}while(false)
-#endif
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-template <typename PointType>
-class SimpleVLPViewer
+void showUsage()
 {
-  public:
-    using Cloud = PointCloud<PointType>;
-    using CloudConstPtr = typename Cloud::ConstPtr;
-    using CloudPtr = typename Cloud::Ptr;
-
-    SimpleVLPViewer (Grabber& grabber,
-                     PointCloudColorHandler<PointType> *handler) :
-        cloud_viewer_ (new PCLVisualizer ("PCL VLP Cloud")),
-        grabber_ (grabber),
-        handler_ (handler)
-    {
-    }
-
-    void
-    cloud_callback (const CloudConstPtr& cloud)
-    {
-      FPS_CALC("cloud callback");
-      std::lock_guard<std::mutex> lock (cloud_mutex_);
-      cloud_ = cloud;
-      printf("points= %ld %dx%d fps=%f\n", cloud_->size(), cloud_->width, cloud_->height, grabber_.getFramesPerSecond());
-    }
-
-    void
-    keyboard_callback (const KeyboardEvent& event,
-                       void* /*cookie*/)
-    {
-      if (event.keyUp ())
-      {
-        switch (event.getKeyCode ())
-        {
-          case '0':
-            delete handler_;
-            handler_ = new PointCloudColorHandlerCustom<PointXYZI> (255, 255, 255);
-            break;
-          case '1':
-            delete handler_;
-            handler_ = new PointCloudColorHandlerGenericField<PointXYZI> ("x");
-            break;
-          case '2':
-            delete handler_;
-            handler_ = new PointCloudColorHandlerGenericField<PointXYZI> ("y");
-            break;
-          case '3':
-            delete handler_;
-            handler_ = new PointCloudColorHandlerGenericField<PointXYZI> ("z");
-            break;
-          case '4':
-            delete handler_;
-            handler_ = new PointCloudColorHandlerGenericField<PointXYZI> ("intensity");
-            break;
-          case 'a':
-            cloud_viewer_->removeAllCoordinateSystems ();
-            cloud_viewer_->addCoordinateSystem (1.0, "global");
-            break;
-          case 'A':
-            cloud_viewer_->removeAllCoordinateSystems ();
-            break;
-        }
-      }
-    }
-
-    void
-    run ()
-    {
-      cloud_viewer_->addCoordinateSystem (1.0, "global");
-      cloud_viewer_->setBackgroundColor (0, 0, 0);
-      cloud_viewer_->initCameraParameters ();
-      cloud_viewer_->setCameraPosition (0.0, 0.0, 30.0, 0.0, 1.0, 0.0, 0);
-      cloud_viewer_->setCameraClipDistances (0.0, 50.0);
-      cloud_viewer_->registerKeyboardCallback (&SimpleVLPViewer::keyboard_callback, *this);
-
-      std::function<void (const CloudConstPtr&)> cloud_cb = [this] (const CloudConstPtr& cloud){ cloud_callback (cloud); };
-      boost::signals2::connection cloud_connection = grabber_.registerCallback (cloud_cb);
-
-      grabber_.start ();
-
-      while (!cloud_viewer_->wasStopped ())
-      {
-        CloudConstPtr tmp, cloud;
-
-        if (cloud_mutex_.try_lock ())
-        {
-          cloud_.swap (cloud);
-          cloud_mutex_.unlock ();
-        }
-
-        if (cloud)
-        {
-          FPS_CALC("drawing cloud");
-          handler_->setInputCloud (cloud);
-          if (!cloud_viewer_->updatePointCloud (cloud, *handler_, "VLP"))
-            cloud_viewer_->addPointCloud (cloud, *handler_, "VLP");
-
-          cloud_viewer_->spinOnce ();
-        }
-
-        if (!grabber_.isRunning ())
-          cloud_viewer_->spin ();
-
-        std::this_thread::sleep_for(100us);
-      }
-
-      grabber_.stop ();
-
-      cloud_connection.disconnect ();
-    }
-
-    PCLVisualizer::Ptr cloud_viewer_;
-    ImageViewer::Ptr image_viewer_;
-
-    Grabber& grabber_;
-    std::mutex cloud_mutex_;
-    std::mutex image_mutex_;
-
-    CloudConstPtr cloud_;
-    PointCloudColorHandler<PointType> *handler_;
-};
-
-void
-usage (char ** argv)
-{
-  std::cout << "usage: " << argv[0] << " [-pcapFile <path-to-pcap-file>] [-h | --help]" << std::endl;
-  std::cout << argv[0] << " -h | --help : shows this help" << std::endl;
-  return;
+	printf("\nUsage:\n"
+			"rtabmap-camera [option] \n"
+			" Options:\n"
+			"    --device #            USB camera device id (default 0).\n"
+			"    --rate #              Frame rate (default 0 Hz). 0 means as fast as possible.\n"
+			"    --path ""             Path to a directory of images or a video file.\n"
+			"    --calibration ""      Calibration file (*.yaml).\n\n");
+	exit(1);
 }
 
-int
-main (int argc,
-      char ** argv)
+int main(int argc, char * argv[])
 {
-  std::string pcapFile;
+	ULogger::setType(ULogger::kTypeConsole);
+	ULogger::setLevel(ULogger::kInfo);
 
-  if (find_switch (argc, argv, "-h") || find_switch (argc, argv, "--help"))
-  {
-    usage (argv);
-    return (0);
-  }
+	int device = 0;
+	std::string path;
+	float rate = 0.0f;
+	std::string calibrationFile;
+	for(int i=1; i<argc; ++i)
+	{
+		if(strcmp(argv[i], "--rate") == 0)
+		{
+			++i;
+			if(i < argc)
+			{
+				rate = uStr2Float(argv[i]);
+				if(rate < 0)
+				{
+					showUsage();
+				}
+			}
+			else
+			{
+				showUsage();
+			}
+			continue;
+		}
+		if(strcmp(argv[i], "--device") == 0)
+		{
+			++i;
+			if(i < argc)
+			{
+				device = std::atoi(argv[i]);
+				if(device < 0)
+				{
+					showUsage();
+				}
+			}
+			else
+			{
+				showUsage();
+			}
+			continue;
+		}
+		if(strcmp(argv[i], "--path") == 0)
+		{
+			++i;
+			if(i < argc)
+			{
+				path = argv[i];
+			}
+			else
+			{
+				showUsage();
+			}
+			continue;
+		}
+		if(strcmp(argv[i], "--calibration") == 0)
+		{
+			++i;
+			if(i < argc)
+			{
+				calibrationFile = argv[i];
+			}
+			else
+			{
+				showUsage();
+			}
+			continue;
+		}
 
-  parse_argument (argc, argv, "-pcapFile", pcapFile);
+		printf("Unrecognized option : %s\n", argv[i]);
+		showUsage();
+	}
 
-  VLPGrabber grabber(boost::asio::ip::address_v4::from_string("192.168.1.201"), 2368);
+	if(path.empty())
+	{
+		UINFO("Using device %d", device);
+	}
+	else
+	{
+		UINFO("Using path %s", path.c_str());
+	}
 
-  auto *color_handler = new PointCloudColorHandlerGenericField<PointXYZI> ("intensity");
+	rtabmap::Camera * camera = 0;
 
-  SimpleVLPViewer<PointXYZI> v (grabber, color_handler);
-  v.run ();
+	if(!path.empty())
+	{
+		if(UFile::exists(path))
+		{
+			if(UFile::getExtension(path).compare("db") == 0)
+			{
+				camera = new rtabmap::DBReader(path, rate);
+			}
+			else
+			{
+				camera = new rtabmap::CameraVideo(path, false, rate);
+			}
+		}
+		else if(UDirectory::exists(path))
+		{
+			camera = new rtabmap::CameraImages(path, rate);
+		}
+		else
+		{
+			UERROR("Path not valid! \"%s\"", path.c_str());
+			return -1;
+		}
+	}
+	else
+	{
+		camera = new rtabmap::CameraVideo(device, false, rate);
+	}
 
-  return (0);
+	if(camera)
+	{
+		if(!calibrationFile.empty())
+		{
+			UINFO("Set calibration: %s", calibrationFile.c_str());
+		}
+		if(!camera->init(UDirectory::getDir(calibrationFile), UFile::getName(calibrationFile)))
+		{
+			delete camera;
+			UERROR("Cannot initialize the camera.");
+			return -1;
+		}
+	}
+
+	cv::Mat rgb;
+	rgb = camera->takeImage().imageRaw();
+	cv::namedWindow("Video", CV_WINDOW_AUTOSIZE); // create window
+	while(!rgb.empty())
+	{
+		cv::imshow("Video", rgb); // show frame
+
+		int c = cv::waitKey(10); // wait 10 ms or for key stroke
+		if(c == 27)
+			break; // if ESC, break and quit
+
+		rgb = camera->takeImage().imageRaw();
+	}
+	cv::destroyWindow("Video");
+	if(camera)
+	{
+		delete camera;
+	}
+	return 0;
 }
