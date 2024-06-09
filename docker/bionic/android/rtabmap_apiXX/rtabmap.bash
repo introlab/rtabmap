ccdddfc83fd23aa362f3daf7fc98723853fbb54d
matlabbe <matlabbe@gmail.com>
2024-06-09 10:19:22 -0700

    Refactored android docker to easily re-use rtabmap.bash in a dev container to install Tango/ARCore/AREngine dependencies.

--- docker/bionic/android/rtabmap_apiXX/rtabmap.bash
+++ docker/bionic/android/rtabmap_apiXX/rtabmap.bash
@@ -9,8 +9,6 @@ fi
 
 prefix=$1
 api=$2
-pwd=$(pwd)
-
 
 # tango
 wget 'https://docs.google.com/uc?authuser=0&id=12rHHkYM5k-UnQn-xGXs9JqYWhSXrgJr3&export=download' -O TangoSDK_Ikariotikos_C.zip
@@ -26,7 +24,7 @@ cp -r lib_tango_support_api/include/* $prefix/arm64-v8a/include/.
 cp -r lib_tango_support_api/lib/arm64-v8a/* $prefix/arm64-v8a/lib/.
 rm -r lib_tango_support_api
 wget 'https://docs.google.com/uc?authuser=0&id=1s5iPJ7xiridj9Jj--gCy2XiQFniheVm6&export=download' -O TangoSDK_Ikariotikos_Java.jar
-mv TangoSDK_Ikariotikos_Java.jar rtabmap-tango/app/android/libs/.
+mv TangoSDK_Ikariotikos_Java.jar app/android/libs/.
 
 # ARCore
 wget 'https://docs.google.com/uc?authuser=0&id=1VsibeqRYpS5pjmrG-vYTXyiPg8kbIfVN&export=download' -O arcore.zip
@@ -34,7 +32,7 @@ unzip -qq arcore.zip
 rm arcore.zip
 cp -r arcore1_18/include/* $prefix/arm64-v8a/include/.
 cp -r arcore1_18/arm64-v8a/* $prefix/arm64-v8a/lib/.
-cp arcore1_18/*.jar rtabmap-tango/app/android/libs/.
+cp arcore1_18/*.jar app/android/libs/.
 rm -r arcore1_18
 
 # AREngine
@@ -43,19 +41,18 @@ unzip -qq arengine.zip
 rm arengine.zip
 cp -r arengine/include/* $prefix/arm64-v8a/include/.
 cp -r arengine/arm64-v8a/* $prefix/arm64-v8a/lib/.
-cp arengine/*.jar rtabmap-tango/app/android/libs/.
+cp arengine/*.jar app/android/libs/.
 rm -r arengine
 
 # resource tool
-cd rtabmap-tango/build
-$pwd/cmake-3.17.0-Linux-x86_64/bin/cmake -DANDROID_PREBUILD=ON ..
+cd build
+/root/cmake-3.17.0-Linux-x86_64/bin/cmake -DANDROID_PREBUILD=ON ..
 make
-cd ../..
 
 # rtabmap
-mkdir rtabmap-tango/build/arm64-v8a
-cd rtabmap-tango/build/arm64-v8a
-$pwd/cmake-3.17.0-Linux-x86_64/bin/cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI=arm64-v8a -DANDROID_NDK=$ANDROID_NDK -DANDROID_NATIVE_API_LEVEL=$api -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$prefix/arm64-v8a -DCMAKE_FIND_ROOT_PATH="$prefix/arm64-v8a/bin;$prefix/arm64-v8a;$prefix/arm64-v8a/share" -DBUILD_EXAMPLES=OFF -DBUILD_TOOLS=OFF -DOpenCV_DIR=$prefix/arm64-v8a/sdk/native/jni ../..
+mkdir arm64-v8a
+cd arm64-v8a
+/root/cmake-3.17.0-Linux-x86_64/bin/cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI=arm64-v8a -DANDROID_NDK=$ANDROID_NDK -DANDROID_NATIVE_API_LEVEL=$api -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$prefix/arm64-v8a -DCMAKE_FIND_ROOT_PATH="$prefix/arm64-v8a/bin;$prefix/arm64-v8a;$prefix/arm64-v8a/share" -DBUILD_EXAMPLES=OFF -DBUILD_TOOLS=OFF -DOpenCV_DIR=$prefix/arm64-v8a/sdk/native/jni ../..
 make
 make clean
 
