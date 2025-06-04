a6b71f471613d2359c3f397d1c99b08d536a6fc1
matlabbe <matlabbe@gmail.com>
2025-06-03 20:37:08 -0700

    docker: remove some COPY to avoid invalidating cache

--- docker/jammy/deps/ros_entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-# setup ros2 environment
-source "/opt/ros/humble/setup.bash" --
-exec "$@"
--- docker/noble-kilted/deps/ros_entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-# setup ros2 environment
-source "/opt/ros/kilted/setup.bash" --
-exec "$@"
