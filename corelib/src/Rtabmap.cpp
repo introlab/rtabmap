bb95cc081acb55f233b4034053934262af2bc54c
matlabbe <matlabbe@gmail.com>
2026-05-27 17:42:21 -0700

    Fixed backward compatibility with bionic (Qt::ItemFlags) (#1710)
    * Fixed backward compatibility with bionic (Qt::ItemFlags)
    
    * dont remove occupancy grid with not publish last signature data

--- corelib/src/Rtabmap.cpp
+++ corelib/src/Rtabmap.cpp
@@ -4418,7 +4418,7 @@ bool Rtabmap::process(
 	}
 	if(!_publishLastSignatureData)
 	{
-		lastSignatureData.sensorData().clearCompressedData();
+		lastSignatureData.sensorData().clearCompressedData(true, true, true, false);
 		lastSignatureData.sensorData().clearRawData();
 	}
 	if(!_rawDataKept)
